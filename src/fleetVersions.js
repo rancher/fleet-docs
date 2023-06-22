70844195966183b48f9d5d9274dcfdb673dba07c
Tim Hardeck <thardeck@suse.com>
2023-06-22 16:09:50 +0200

    Remove "Next" version download references
    because we are using Artifact Hub instead.

--- src/fleetVersions.js
+++ src/fleetVersions.js
@@ -10,9 +10,6 @@ export const versions = {
     "fleetCRD": "https://github.com/rancher/fleet/releases/download/v0.6.0/fleet-crd-0.6.0.tgz",
   },
   'next': {
-    "fleet": "https://github.com/rancher/fleet/releases/download/v0.7.0-AGENT-rc.1/fleet-0.7.0-AGENT-rc.1.tgz",
-    "fleetAgent": "https://github.com/rancher/fleet/releases/download/v0.7.0-AGENT-rc.1/fleet-agent-0.7.0-AGENT-rc.1.tgz",
-    "fleetCRD": "https://github.com/rancher/fleet/releases/download/v0.7.0-AGENT-rc.1/fleet-crd-0.7.0-AGENT-rc.1.tgz",
     "kubernetes": "1.20.5",
   },
 }
